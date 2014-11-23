e2aafff4940760d2b6ee92179a8e3c21285dc92f
Martin Freidank <martin@mars>
2014-11-23 23:18:48 +0100

    fixed typo

--- toxsync.sh
+++ toxsync.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
-ID= #PGP ID
-TOXFILE="~/.config/tox/myfile.tox"; #Path to Your Tox file
-BACKUP="~/backup.asc"; #Path to your Backup File
+ID=""; #PGP ID
+TOXFILE="~/.config/tox/93C482B6DAA2.tox"; #Path to Your Tox file
+BACKUP="~/ownCloud/toxbackup.asc"; #Path to your Backup File
 CLIENT="qtox"; #Tox start command, e.g. qtox, utox...
 
 
 ################################  Don't change anything below this line unless you know what you are doing
 gpg -o "$TOXFILE" -d "$BACKUP";
 $CLIENT;
-gpg -r "$ID$" -o "$BACKUP" -e "$TOXFILE"
+gpg -r "$ID" -o "$BACKUP" -e "$TOXFILE"
 
 
 exit 0
