692eceb3e94d3d868dcf2ef46db4afab2ca0cbc2
Martin Freidank <martin@mars>
2014-11-27 16:56:59 +0100

    Automatically overwrite files processed by gpg

--- toxsync.sh
+++ toxsync.sh
@@ -6,9 +6,9 @@ CLIENT="qtox"; #Tox start command, e.g. qtox, utox...
 
 
 ################################  Don't change anything below this line unless you know what you are doing
-gpg -o "$TOXFILE" -d "$BACKUP";
+gpg --yes -o "$TOXFILE" -d "$BACKUP";
 $CLIENT;
-gpg -r "$ID" -o "$BACKUP" -e "$TOXFILE"
+gpg --yes -r "$ID" -o "$BACKUP" -e "$TOXFILE"
 
 
 exit 0
