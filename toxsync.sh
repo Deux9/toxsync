e6db849e3fbe509c3478f1c4f13da70e2cedce7a
Martin Freidank <martinfreidank@openmailbox.org>
2014-12-05 23:44:57 +0100

    updated

--- toxsync.sh
+++ toxsync.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 ID=""; #PGP ID
-TOXFILE="~/.config/tox/93C482B6DAA2.tox"; #Path to Your Tox file
+TOXFILE=""; #Path to Your Tox file
 BACKUP="~/ownCloud/toxbackup.asc"; #Path to your Backup File
 CLIENT="qtox"; #Tox start command, e.g. qtox, utox...
 
