12652fcb6c1a2a2e8d25b6220b1199754da3f158
Martin Freidank <martin@mars>
2015-01-08 18:50:53 +0100

    cleaned up

--- toxsync.sh
+++ toxsync.sh
@@ -1,11 +1,14 @@
 #!/bin/bash
-ID=71DCFC4C; #PGP ID
-TOXFILE=~/.config/tox/neckbeardless.tox; #Path to Your Tox file
-BACKUP=~/ownCloud/toxbackup.asc; #Path to your Backup File
-CLIENT=qtox; #Tox start command, e.g. qtox, utox...
+ID="111111"; #PGP ID
+TOXFILE="~/.config/tox/2222222222.tox"; #Path to Your Tox file
+BACKUP="~/ownCloud/toxbackup.asc"; #Path to your Backup File
+CLIENT="qtox"; #Tox start command, e.g. qtox, utox...
 
-################################ Don't change anything below this line unless you know what you are doing
-gpg --yes -o $TOXFILE -d $BACKUP;
+
+################################  Don't change anything below this line unless you know what you are doing
+gpg --yes -o "$TOXFILE" -d "$BACKUP";
 $CLIENT;
-gpg --yes -r $ID -o $BACKUP -e $TOXFILE
+gpg --yes -r "$ID" -o "$BACKUP" -e "$TOXFILE"
+
+
 exit 0
